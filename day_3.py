23757aeec3407950ed53117a0f6aeddaec1c04ff
Darren Butler <butlerdarren18@gmail.com>
2025-12-14 07:13:55 -0800

    converted to ints.. feel like this one is going to go fast.

--- day_3.py
+++ day_3.py
@@ -2,5 +2,26 @@
 # largest possible number each line. then sum.
 
 input_file = open("day_3_example_inputs.txt")
-input_text = input_file.read()
+input_lines = input_file.readlines()
 
+
+# remove empty spaces
+
+new_lines = []
+
+for line in input_lines: 
+	holder = []
+	for char in line: 
+		if char != "\n": holder.append(char)
+	new_lines.append(holder)
+
+input_lines = new_lines
+
+
+# convert to int 
+ints = []
+for line in input_lines: 
+	string = "".join(line)
+	ints.append(int(string))
+
+print(ints)
