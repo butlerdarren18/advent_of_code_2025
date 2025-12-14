efcd48dd47a174f9ba17474c9441e65a59e07d75
Darren Butler <butlerdarren18@gmail.com>
2025-12-14 07:46:47 -0800

    this one is definitely easier. Got the pairs already

--- day_3.py
+++ day_3.py
@@ -17,11 +17,22 @@ for line in input_lines:
 
 input_lines = new_lines
 
+# determine two highest nums 
 
-# convert to int 
-ints = []
-for line in input_lines: 
-	string = "".join(line)
-	ints.append(int(string))
 
-print(ints)
+for line in input_lines:
+	highest_indx = -2
+	second_highest_indx = -1
+
+	# pick the highest number 
+	for i in range(len(line)):
+		if line[i] >= line[highest_indx] and i < len(line)-1: highest_indx = i
+	# pick the second highest number 
+	for i in range(len(line)): 
+		if i > highest_indx: 
+			if line[i] >= line[second_highest_indx]: second_highest_indx = i
+
+
+	print(line[highest_indx], line[second_highest_indx])
+
+
