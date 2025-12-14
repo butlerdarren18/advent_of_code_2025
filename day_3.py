9b718a4619e77e9b3d889c2f7d3baafab7b4f30f
Darren Butler <butlerdarren18@gmail.com>
2025-12-14 07:07:10 -0800

    started day 3

---
+++ day_3.py
@@ -0,0 +1,6 @@
+# you can pick two numeralsfrom each line. They are combined to form a number and cannot be rearranged. 
+# largest possible number each line. then sum.
+
+input_file = open("day_3_example_inputs.txt")
+input_text = input_file.read()
+
